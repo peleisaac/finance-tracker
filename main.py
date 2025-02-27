ae708118f28cfdc6c46caf6bf796972b6ce49c71
peleisaac <151077839+peleisaac@users.noreply.github.com>
2025-02-27 15:48:23 +0000

    Updated main.py file with run function to check the display of the add_transaction function and the display_menu

--- main.py
+++ main.py
@@ -1,1 +1,42 @@
+from finance import FinanceTracker
+
+
+class MainMenu:
+    def __init__(self):
+        self.tracker = FinanceTracker()
+        
+    
+    def display_menu(self):
+        menu = """
+        
+        Welcome to your Finance Tracker Application.
+        Please select an option below:
+        ========================
+        
+        1. Add Transactions
+        
+        0. Exit
+        ========================
+        """
+        print(menu)
+        
+    def run(self):
+        while True:
+            self.display_menu()
+            choice = input("Enter your choice: ")
+            if choice == "1":
+                self.tracker.add_transaction()
+                
+            elif choice == "0":
+                print("Exiting application. Goodbye!")
+                break
+            else:
+                print("Invalid choice. Please try again.")
+
+
+if __name__ == "__main__":
+    mainmenu = MainMenu()
+    mainmenu.run()
+
+Main.py
 
