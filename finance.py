dc84d726baa29cef23c0b346a6094a0f084b9419
peleisaac <151077839+peleisaac@users.noreply.github.com>
2025-02-27 15:46:16 +0000

    Update finance.py with the add_transaction function

--- finance.py
+++ finance.py
@@ -1,1 +1,25 @@
+class FinanceTracker:
+    def add_transaction(self):
+        try:
+            print(f"\nAdding a new transaction")
+            transaction_type = input("Enter type (income/expense): ").lower()
+            if transaction_type not in ["income", "expense"]:
+                raise ValueError(
+                    "Invalid transaction type. Choose 'income' or 'expense'."
+                )
+            
+            date = input(
+                "Enter the date (YYYY-MM-DD) or press enter to ue today's date: "
+            )
+
+            amount = float(input("Enter amount: "))
+
+            description = input("Enter description: ")
+            category = input("Enter category: ")
+            print(f"{date, amount, description, category, transaction_type}")
+            print("\nTransaction added successfully!\n")
+                
+        except Exception as e:
+            print(f"\nAn error occurred: {e}\n")
+            
 
